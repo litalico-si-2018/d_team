d5c02ef3210b1dee704e288fe6a8519914f5dba3
hamasugar <hamazi511@gmail.com>
2018-08-11 18:45:07 +0900

    change problem class

--- app/src/main/java/jp/summerintern/sushi/sushi/MainActivity.kt
+++ app/src/main/java/jp/summerintern/sushi/sushi/MainActivity.kt
@@ -8,17 +8,66 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var time: Date
     private lateinit var sushiList: List<String>
+    private lateinit var ansList:List<Int>
+    private lateinit var probs:List<String>
+    private  var isTrue:Int = 0
+    private  lateinit var prob:String
+    private  var level:Int = 0
+    private  var submitList =  arrayOfNulls<Int>(level)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun judge(val probs: List<Problem>)
+    fun judge():Boolean {
 
-    fun showOneResult()
 
-    fun genProbs()
+        if (ansList == submitList ){
+            return true
+
+        }
+        else{
+            return false
+        }
+
+
+
+    }
+
+    fun showOneResult(){
+
+
+
+
+    }
+
+
+
+    fun genProbs(level: Int) {
+
+        for (i in 0..level) {
+            isTrue = Random().nextInt(1)
+            if (isTrue == 0){
+                val problem = Problem("aaa")
+                prob = problem.genSentence(true)
+                probs.plus(prob)
+                ansList.plus(0)
+            }
+
+            else{
+                val problem = Problem("aaa")
+                prob = problem.genSentence(false)
+                probs.plus(prob)
+                ansList.plus(1)
+
+            }
+
+
+        }
+
+
+    }
 
     fun updateResults()
 }
--- app/src/main/java/jp/summerintern/sushi/sushi/Problem.kt
+++ app/src/main/java/jp/summerintern/sushi/sushi/Problem.kt
@@ -1,5 +1,32 @@
 package jp.summerintern.sushi.sushi
 
-class Problem(val sentence: String, val ans_flag: Boolean, val level: Int) {
-    fun genSentence()
+import java.util.*
+
+class Problem(var sentence: String) {
+
+
+
+    private var leftNumber:Int = 0
+    private var rightNumber:Int = 0
+
+
+    fun genSentence(ans_flag: Boolean): String {
+
+        leftNumber = Random().nextInt(8) + 1
+        rightNumber = Random().nextInt(8) + 1
+
+        if (ans_flag == true){
+
+            sentence = "${leftNumber} + ${rightNumber} = ${leftNumber + rightNumber}"
+
+        }
+        else {
+            sentence = "${leftNumber} - ${rightNumber} = ${leftNumber + rightNumber}"
+        }
+
+        return sentence
+
+    }
+
+
 }
